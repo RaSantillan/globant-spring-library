be0cf62841936621dd382801b5fc6f09488c5d20
RaSantillan <rodrialesanti@hotmail.com>
2024-02-05 15:46:54 -0500

    v3 Create ImageRepository and ImageService
    ImageRepository is just an interface that extends JPA repository.
    Image service has a method to save an image and a method to update
    an image.

---
+++ src/main/java/com/exercises/library/repository/ImageRepository.java
@@ -0,0 +1,9 @@
+package com.exercises.library.repository;
+
+import com.exercises.library.entity.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ImageRepository extends JpaRepository<Image, String> {
+}
