0b9a0f26b4134a2ce01a09f53426c79af8afe875
RaSantillan <rodrialesanti@hotmail.com>
2024-02-06 01:52:12 -0500

    v3 Modify UserService to add Image parameter
    Added to registrar method the Image parameter.
    Created the update method to modify an existing user.

--- src/main/java/com/exercises/library/service/UserService.java
+++ src/main/java/com/exercises/library/service/UserService.java
@@ -1,5 +1,6 @@
 package com.exercises.library.service;
 
+import com.exercises.library.entity.Image;
 import com.exercises.library.entity.User;
 import com.exercises.library.enumeration.Role;
 import com.exercises.library.exception.MyException;
@@ -16,28 +17,68 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService implements UserDetailsService {
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final ImageService imageService;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, ImageService imageService) {
         this.userRepository = userRepository;
+        this.imageService = imageService;
     }
 
     @Transactional
-    public void registrar(String name, String email, String password, String password2) throws MyException {
+    public void registrar(
+            String name, String email, String password, String password2, MultipartFile file) throws MyException {
         validar(name, email, password, password2);
         User user = new User();
         user.setName(name);
         user.setEmail(email);
         user.setPassword(new BCryptPasswordEncoder().encode(password));
         user.setRole(Role.USER);
+
+        Image image = imageService.save(file);
+        user.setImage(image);
+
         userRepository.save(user);
+    }
+
+    @Transactional
+    public void update(
+            String id, String name, String email, String password, String password2, MultipartFile file
+    ) throws MyException {
+        validar(name, email, password, password2);
+
+        Optional<User> response = userRepository.findById(id);
+        if (response.isPresent()) {
+
+            User usuario = response.get();
+            usuario.setName(name);
+            usuario.setEmail(email);
+
+            usuario.setPassword(new BCryptPasswordEncoder().encode(password));
+
+            usuario.setRole(Role.USER);
+
+            String idImage = null;
+
+            if (usuario.getImage() != null) {
+                idImage = usuario.getImage().getId();
+            }
+
+            Image image = imageService.update(file, idImage);
+
+            usuario.setImage(image);
+
+            userRepository.save(usuario);
+        }
     }
 
     private void validar(String nombre, String email, String password, String password2) throws MyException {
