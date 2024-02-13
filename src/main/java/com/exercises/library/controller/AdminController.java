dc73734e05a99928556a6ce150259e7e02961cc0
RaSantillan <rodrialesanti@hotmail.com>
2024-02-13 18:31:38 -0500

    feat: Add GetMapping methods to AdminController
    -listar method
    -cambiar rol method

--- src/main/java/com/exercises/library/controller/AdminController.java
+++ src/main/java/com/exercises/library/controller/AdminController.java
@@ -1,15 +1,44 @@
 package com.exercises.library.controller;
 
+import com.exercises.library.entity.User;
+import com.exercises.library.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+    private final UserService userService;
+
+    @Autowired
+    public AdminController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/dashboard")
     public String panelAdministrativo() {
         return "panel.html";
+    }
+
+    @GetMapping("/usuarios")
+    public String listar(ModelMap modelo) {
+        List<User> users = userService.listarUsuarios();
+        modelo.addAttribute("usuarios", users);
+
+        return "usuario_list";
+    }
+
+    @GetMapping("/modificarRol/{id}")
+    public String cambiarRol(@PathVariable String id){
+        userService.cambiarRol(id);
+
+        return "redirect:/admin/usuarios";
     }
 }
