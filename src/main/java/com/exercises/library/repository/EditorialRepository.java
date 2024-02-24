b6c5b820ce163b6209d953bc3e2af948cfd08a85
RaSantillan <rodrialesanti@hotmail.com>
2024-02-24 15:16:20 -0500

    feat(Editorial): added new method and refactored code

--- src/main/java/com/exercises/library/repository/EditorialRepository.java
+++ src/main/java/com/exercises/library/repository/EditorialRepository.java
@@ -3,7 +3,5 @@ package com.exercises.library.repository;
 import com.exercises.library.entity.Editorial;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
-
 public interface EditorialRepository extends JpaRepository<Editorial, String> {
 }
